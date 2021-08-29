d0a42656da6c9132962c81c7e33eb5cef4f67d5c
Rich E <rich.eakin@gmail.com>
2021-08-29 14:04:59 -0400

    HelloTriangle test is building and linking to both diligent and cinder

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Personal workspace for experimenting with [DiligentEngine](https://github.com/DiligentGraphics/DiligentEngine).
+
+
+Building:
+```
+cmake -S . -B ./build/Win64 -G "Visual Studio 16 2019"
+```
---
+++ test/HelloTriangle/src/HelloTriangleApp.cpp
@@ -0,0 +1,192 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/ImageIo.h"
+#include "cinder/Log.h"
+
+// included from DiligentEngine\DiligentSamples\SampleBase\include\SampleBase.hpp
+#include "EngineFactory.h"
+#include "RefCntAutoPtr.hpp"
+#include "RenderDevice.h"
+#include "DeviceContext.h"
+#include "SwapChain.h"
+//#include "InputController.hpp"
+#include "BasicMath.hpp"
+
+using namespace ci;
+using namespace Diligent;
+
+static const char* VSSource = R"(
+struct PSInput 
+{ 
+    float4 Pos   : SV_POSITION; 
+    float3 Color : COLOR; 
+};
+
+void main(in  uint    VertId : SV_VertexID,
+          out PSInput PSIn) 
+{
+    float4 Pos[3];
+    Pos[0] = float4(-0.5, -0.5, 0.0, 1.0);
+    Pos[1] = float4( 0.0, +0.5, 0.0, 1.0);
+    Pos[2] = float4(+0.5, -0.5, 0.0, 1.0);
+
+    float3 Col[3];
+    Col[0] = float3(1.0, 0.0, 0.0); // red
+    Col[1] = float3(0.0, 1.0, 0.0); // green
+    Col[2] = float3(0.0, 0.0, 1.0); // blue
+
+    PSIn.Pos   = Pos[VertId];
+    PSIn.Color = Col[VertId];
+}
+)";
+
+// Pixel shader simply outputs interpolated vertex color
+static const char* PSSource = R"(
+struct PSInput 
+{ 
+    float4 Pos   : SV_POSITION; 
+    float3 Color : COLOR; 
+};
+
+struct PSOutput
+{ 
+    float4 Color : SV_TARGET; 
+};
+
+void main(in  PSInput  PSIn,
+          out PSOutput PSOut)
+{
+    PSOut.Color = float4(PSIn.Color.rgb, 1.0);
+}
+)";
+
+#if 0
+void Tutorial01_HelloTriangle::Initialize(const SampleInitInfo& InitInfo)
+{
+    SampleBase::Initialize(InitInfo);
+
+    // Pipeline state object encompasses configuration of all GPU stages
+
+    GraphicsPipelineStateCreateInfo PSOCreateInfo;
+
+    // Pipeline state name is used by the engine to report issues.
+    // It is always a good idea to give objects descriptive names.
+    PSOCreateInfo.PSODesc.Name = "Simple triangle PSO";
+
+    // This is a graphics pipeline
+    PSOCreateInfo.PSODesc.PipelineType = PIPELINE_TYPE_GRAPHICS;
+
+    // clang-format off
+    // This tutorial will render to a single render target
+    PSOCreateInfo.GraphicsPipeline.NumRenderTargets             = 1;
+    // Set render target format which is the format of the swap chain's color buffer
+    PSOCreateInfo.GraphicsPipeline.RTVFormats[0]                = m_pSwapChain->GetDesc().ColorBufferFormat;
+    // Use the depth buffer format from the swap chain
+    PSOCreateInfo.GraphicsPipeline.DSVFormat                    = m_pSwapChain->GetDesc().DepthBufferFormat;
+    // Primitive topology defines what kind of primitives will be rendered by this pipeline state
+    PSOCreateInfo.GraphicsPipeline.PrimitiveTopology            = PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    // No back face culling for this tutorial
+    PSOCreateInfo.GraphicsPipeline.RasterizerDesc.CullMode      = CULL_MODE_NONE;
+    // Disable depth testing
+    PSOCreateInfo.GraphicsPipeline.DepthStencilDesc.DepthEnable = False;
+    // clang-format on
+
+    ShaderCreateInfo ShaderCI;
+    // Tell the system that the shader source code is in HLSL.
+    // For OpenGL, the engine will convert this into GLSL under the hood.
+    ShaderCI.SourceLanguage = SHADER_SOURCE_LANGUAGE_HLSL;
+    // OpenGL backend requires emulated combined HLSL texture samplers (g_Texture + g_Texture_sampler combination)
+    ShaderCI.UseCombinedTextureSamplers = true;
+    // Create a vertex shader
+    RefCntAutoPtr<IShader> pVS;
+    {
+        ShaderCI.Desc.ShaderType = SHADER_TYPE_VERTEX;
+        ShaderCI.EntryPoint      = "main";
+        ShaderCI.Desc.Name       = "Triangle vertex shader";
+        ShaderCI.Source          = VSSource;
+        m_pDevice->CreateShader(ShaderCI, &pVS);
+    }
+
+    // Create a pixel shader
+    RefCntAutoPtr<IShader> pPS;
+    {
+        ShaderCI.Desc.ShaderType = SHADER_TYPE_PIXEL;
+        ShaderCI.EntryPoint      = "main";
+        ShaderCI.Desc.Name       = "Triangle pixel shader";
+        ShaderCI.Source          = PSSource;
+        m_pDevice->CreateShader(ShaderCI, &pPS);
+    }
+
+    // Finally, create the pipeline state
+    PSOCreateInfo.pVS = pVS;
+    PSOCreateInfo.pPS = pPS;
+    m_pDevice->CreateGraphicsPipelineState(PSOCreateInfo, &m_pPSO);
+}
+
+// Render a frame
+void Tutorial01_HelloTriangle::Render()
+{
+    // Clear the back buffer
+    const float ClearColor[] = {0.350f, 0.350f, 0.350f, 1.0f};
+    // Let the engine perform required state transitions
+    auto* pRTV = m_pSwapChain->GetCurrentBackBufferRTV();
+    auto* pDSV = m_pSwapChain->GetDepthBufferDSV();
+    m_pImmediateContext->ClearRenderTarget(pRTV, ClearColor, RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+    m_pImmediateContext->ClearDepthStencil(pDSV, CLEAR_DEPTH_FLAG, 1.f, 0, RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+
+    // Set the pipeline state in the immediate context
+    m_pImmediateContext->SetPipelineState(m_pPSO);
+
+    // Typically we should now call CommitShaderResources(), however shaders in this example don't
+    // use any resources.
+
+    DrawAttribs drawAttrs;
+    drawAttrs.NumVertices = 3; // We will render 3 vertices
+    m_pImmediateContext->Draw(drawAttrs);
+}
+
+void Tutorial01_HelloTriangle::Update(double CurrTime, double ElapsedTime)
+{
+    SampleBase::Update(CurrTime, ElapsedTime);
+}
+
+#endif
+
+class HelloTriangleApp : public app::App {
+  public:   
+    void    setup() override;
+    void    resize() override;
+    void    update() override;
+    void    draw() override;
+    
+};
+
+void HelloTriangleApp::setup()
+{
+    // Pipeline state object encompasses configuration of all GPU stages
+
+    GraphicsPipelineStateCreateInfo PSOCreateInfo;
+
+    // Pipeline state name is used by the engine to report issues.
+    // It is always a good idea to give objects descriptive names.
+    PSOCreateInfo.PSODesc.Name = "Simple triangle PSO";
+
+    // This is a graphics pipeline
+    PSOCreateInfo.PSODesc.PipelineType = PIPELINE_TYPE_GRAPHICS;
+}
+
+void HelloTriangleApp::resize()
+{
+}
+
+void HelloTriangleApp::update()
+{
+}
+
+void HelloTriangleApp::draw()
+{
+}
+
+CINDER_APP( HelloTriangleApp, app::RendererGl( app::RendererGl::Options().msaa( 16 ) ) )
+
